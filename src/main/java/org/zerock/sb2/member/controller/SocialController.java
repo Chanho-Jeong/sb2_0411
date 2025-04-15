64d06ea439e23a75d7942540fc68cacd94575af7
zerock <kangckck24@gmail.com>
2025-04-15 15:36:15 +0900

    Todo Config

--- src/main/java/org/zerock/sb2/member/controller/SocialController.java
+++ src/main/java/org/zerock/sb2/member/controller/SocialController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class SocialController {
 
-    @RequestMapping("/api/v1/member/kakao")
+    @GetMapping("/api/v1/member/kakao")
     public ResponseEntity<String[]> getKakao( @RequestParam("accessToken") String accessToken) {
 
         log.info("getKakao: " + accessToken);
